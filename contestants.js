215973e20ad19a70560094eff2f7ad1e6703793a
Khoa Thien Le (Harry) <khoatle165@gmail.com>
2020-07-12 02:29:29 -0400

    add season info and individual stat on original season of the contestants

--- contestants.js
+++ contestants.js
@@ -14,9 +14,9 @@ module.exports = () => {
       );
 
       const data = {};
-      let run = false;
+      let runOnce = false;
       startingPoints.each(function (i, ele) {
-        if (!run) {
+        if (!runOnce) {
           // Italic tag indicates the contestant has passed away.
           const italicTagFound = $(ele).find('i').length;
 
@@ -29,29 +29,84 @@ module.exports = () => {
           const birthdayStringData = JSON.stringify(birthdayRef.attribs);
           const birthdayJsonData = JSON.parse(birthdayStringData);
 
+          // ---------------- Filming age ----------------
           const filmingAgeRef = birthdayRef.nextSibling;
-          const filmingAge = filmingAgeRef.text().trim();
 
-          console.log(filmingAge);
+          // ---------------- Hometown ----------------
+          const hometownRef = filmingAgeRef.nextSibling;
+
+          // ---------------- Season number ----------------
+          const seasonRef = hometownRef.nextSibling;
+          const seasonNumberStringData = JSON.stringify(seasonRef.attribs);
+          const seasonNumberJsonData = JSON.parse(seasonNumberStringData);
+
+          // ---------------- Season name ----------------
+          const seasonNameStringData = JSON.stringify(
+            $(seasonRef).find('a')[0].attribs
+          );
+          const seasonNameJsonData = JSON.parse(seasonNameStringData);
+
+          // ---------------- Finish placement ----------------
+          const finishPlacementRef = seasonRef.nextSibling;
+
+          // ---------------- Days lasted ----------------
+          const daysLastedRef = finishPlacementRef.nextSibling;
+
+          // ---------------- Votes against ----------------
+          const votesAgainstRef = daysLastedRef.nextSibling;
+
+          // ---------------- Tribal wins ----------------
+          const tribalWinsRef = votesAgainstRef.nextSibling;
+
+          // ---------------- Individual wins ----------------
+          const individualWinsRef = tribalWinsRef.nextSibling;
 
           const href = site + nameJsonData.href;
           const name = nameJsonData.title;
           const birthday = birthdayJsonData['data-sort-value'];
           const passedAway = italicTagFound ? 'true' : 'false';
+          const filmingAge = parseInt(filmingAgeRef.firstChild.data.trim());
+          const hometown = hometownRef.firstChild.data.trim();
+          const seasonNumber = parseInt(
+            seasonNumberJsonData['data-sort-value'],
+            10
+          );
+          const seasonHref = site + seasonNameJsonData.href;
+          const seasonName = seasonNameJsonData.title;
+          const finishPlacement = finishPlacementRef.firstChild.data.trim();
+          const daysLasted = parseInt(daysLastedRef.firstChild.data.trim());
+          const votesAgainst = votesAgainstRef.firstChild.data.trim();
+          const tribalWins = tribalWinsRef.firstChild.data.trim();
+          const individualWins = individualWinsRef.firstChild.data.trim();
 
-          const contestant = { href, name, birthday, passedAway };
+          const contestant = {
+            href,
+            name,
+            birthday,
+            passedAway,
+            filmingAge,
+            hometown,
+            seasonNumber,
+            seasonHref,
+            seasonName,
+            finishPlacement,
+            daysLasted,
+            votesAgainst,
+            tribalWins,
+            individualWins,
+          };
           const key = nameJsonData.href.split('/wiki/')[1];
 
           data[key] = contestant;
-          run = true;
+          // runOnce = true;
         }
       });
 
       const formattedData = JSON.stringify(data, null, 2);
 
-      // fs.writeFile('./data/contestants.json', formattedData, (err) => {
-      //   if (err) console.log(err);
-      // });
+      fs.writeFile('./data/contestants.json', formattedData, (err) => {
+        if (err) console.log(err);
+      });
     })
     .catch((err) => {
       console.log(err);
